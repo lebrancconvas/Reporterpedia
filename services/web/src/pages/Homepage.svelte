cfcc80de439e27e939c741247b850ae70f674f78
lebrancconvas <lebrancconvas@gmail.com>
2022-06-29 19:48:54 +0700

    ✨ Create Router.

---
+++ services/web/src/pages/Homepage.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+
+</script>
+
+<main>
+  <header>
+    <div class="heading">
+      <h1>Reporterpedia</h1>
+    </div>
+    <div class="bio">
+      <h3>Know about the reporter as their reports</h3>
+    </div>
+  </header>
+  <section class="national">
+    <div class="grid-container">
+      <div class="grid-item">
+        <h3>Chinese</h3>
+      </div>
+      <div class="grid-item">
+        <h3>Korean</h3>
+      </div>
+      <div class="grid-item">
+        <h3>Italian</h3>
+      </div>
+      <div class="grid-item">
+        <h3>Latvian</h3>
+      </div>
+      <div class="grid-item">
+        <h3>France</h3>
+      </div>
+    </div>
+  </section>
+</main>
+
+<style>
+  main {
+    font-family: 'Playfair Display', serif;
+  }
+  header {
+    text-align: center;
+  }
+  .grid-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .grid-item {
+    margin: 0 50px;
+  }
+</style>
